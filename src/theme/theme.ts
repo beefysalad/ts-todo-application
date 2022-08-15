ead4ed72a9f737464edc7d4fe53c1e0531f785b7
John Patrick <jpatrickzephyr@gmail.com>
2022-08-15 14:25:54 +0800

    chore: update theme

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,13 +1,39 @@
 import { extendTheme, type ThemeConfig } from '@chakra-ui/react';
+import type { StyleFunctionProps } from '@chakra-ui/styled-system';
+import { mode } from '@chakra-ui/theme-tools';
 
 const config: ThemeConfig = {
   initialColorMode: 'light',
   useSystemColorMode: false,
 };
+
 const theme = extendTheme(config, {
+  styles: {
+    global: (props: StyleFunctionProps) => ({
+      body: {
+        color: mode('gray.800', 'whiteAlpha.900')(props),
+        bg: mode('gray.50', 'gray.800')(props),
+      },
+    }),
+  },
   fonts: {
     heading: `'Poppins', sans-serif`,
-    body: `'Poppins', sans-serif`,
+    body: `'Inter', sans-serif`,
+    mono: 'Menlo, monospace',
+  },
+  components: {
+    Input: {
+      variants: {
+        filled: (props: StyleFunctionProps) => ({
+          field: {
+            borderRadius: 'md',
+            _focus: {
+              borderColor: 'teal.500',
+            },
+          },
+        }),
+      },
+    },
   },
 });
 
