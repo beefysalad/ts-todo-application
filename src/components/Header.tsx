f0d9537e0a2c704548ff0b078fb80f97b003ca16
John Patrick <jpatrickzephyr@gmail.com>
2022-08-15 18:17:06 +0800

    chore: add testLogo on header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,16 +4,19 @@ import {
   Heading,
   HStack,
   IconButton,
+  Image,
   Link,
   Spacer,
   Tooltip,
   useColorMode,
+  useColorModeValue,
 } from '@chakra-ui/react';
 import { MdInvertColors } from 'react-icons/md';
-
+import logo from '../assets/logo.png';
 function Header() {
   const BEEFY_GITHUB_URL = 'https://github.com/beefysalad';
   const { colorMode, toggleColorMode } = useColorMode();
+  const bg = useColorModeValue('gray.50', 'gray.800');
   return (
     <Box
       zIndex='1'
@@ -21,14 +24,15 @@ function Header() {
       position='fixed'
       top='0'
       left='0'
-      bg={colorMode}
+      bg={bg}
       boxShadow='md'
     >
       <Flex alignItems='center' px={{ base: '3', md: '5', lg: '8' }} py='3'>
         <Box>
           <Heading size='md'>
             <HStack>
-              <Link>BeefySalad</Link>
+              <Image src={logo} h='10' />
+              <Link _hover={{ underline: 'none' }}>BeefySalad</Link>
             </HStack>
           </Heading>
         </Box>
